ba7e418258e1c0e53d0a8c11fb54f3e48bbb1e35
Tony Mudau <mudau7tony3@gmail.com>
2021-08-28 01:33:10 +0200

    changed form and added checkbox

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, send_file
 from forms import PersonalDetails
+
+
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'LOLU'
 
@@ -25,14 +27,42 @@ def create_cv():
     from forms import PersonalDetails
     form = PersonalDetails()
 
-    # Form data to create cv 
+    # Form data to create cv
+    #PERSONAL_DETAILS
     name = request.form['name']
     last_name = request.form['last_name']
     street = request.form['street']
     suburb = request.form['suburb']
     city = request.form['city']
     province = request.form['province']
-    #email_address = request.form['email_address']
+    DOB = request.form['DOB']
+    email_address = request.form['email_address']
+    cell_num = request.form['cell_num']
+    #When making checkboxes use exception
+    try:
+        html = request.form['html']
+    except:
+        html = ''
+    try:
+        css = request.form['css']
+    except:
+         css = ''
+    try:
+        python = request.form['python']
+    except:
+        python = ''
+    try:
+        java = request.form['Java']
+    except:
+        java = ''
+    try:
+        javascript = request.form['javascript']
+    except:
+        javascript = ''
+    try:
+        sql = request.form['SQL']
+    except:
+        sql = ''
     
 
     # CODE
@@ -45,31 +75,44 @@ def create_cv():
             # Move to the right
             self.cell(66)
             # Title
-            self.cell(30, 20, f'Curriculum Vitae of {name}', border=False, ln=1)
+            self.cell(30, 20, f'{name} {last_name}', border=False, ln=1)
             # Line break
             self.ln(10)
-
+        
+        #For creating a headding
         def heading(self, heading_title):
-            self.set_font('Helvetica', '', 11)
+            self.set_font('Helvetica', '', 12 )
             #start, h, legnth, h
-            self.set_text_color(0, 0, 255)
+            self.set_text_color(0,0,255)
             self.cell(10)
             self.cell(0, 0, heading_title)
-            self.line(21, 44, 190, 44)
             self.ln(10)
 
+        def lines(self, i,s,l,e):
+            self.line(i, s, l, e)
+
+
+
         def text(self, ans):
-            self.set_font('Times', '', 12)
+            self.set_font('Helvetica', '', 10)
             self.set_text_color(0, 0, 0)
             self.cell(10)
-            self.cell(0, 0, ans)
+            self.multi_cell(0, 0, ans)
             self.ln(10)
+
+        def paragraph(self, para):
+            self.set_font('Helvetica', '', 10)
+            self.set_text_color(0, 0, 0)
+            self.cell(10)
+            self.multi_cell(0, 4, para)
+            self.ln(10)
+
 
         def footer(self):
             # Position at 1.5 cm from bottom
             self.set_y(-15)
             # Arial italic 8
-            self.set_font('Times', 'I', 10)
+            self.set_font('Helvetica', 'I', 10)
             # Text color in gray
             self.set_text_color(128)
             # Page number
@@ -85,23 +128,25 @@ def create_cv():
 
     # CV Page
     pdf.heading('Personal details')
-    pdf.text(f'First Names:     {name}')
-    pdf.text(f'Last Names:      {last_name}')
-    pdf.text(f'Residential Area:    {street}, {suburb}, {city}, {province}')
-    #pdf.text(f'Email Address: {email_address}')
-    # pdf.text(f'Cell Numbers: {cell_num}')
-    # pdf.text(f'Drivers License: {drivers_license}')
-    #
-    # pdf.heading('Links')
-    # pdf.text(f'Personal Projects')
-    # pdf.text(f'')
-    # pdf.text(f'')
-    # pdf.text(f'')
+    pdf.lines(21, 44, 190, 44)
+    pdf.text(f'Contact number:                 {cell_num}')
+    pdf.text(f'Email address:                    {email_address}')
+    pdf.paragraph(f"""Date of Birth:                      {DOB}
+Residential Area:               {street}, {suburb}, {city}, {province}""")
+    pdf.heading('About me')
+    pdf.lines(21,80,190,80)
+    pdf.paragraph(f"""I am an honest and hard-working individual who believes in quality work, continuous learning and sharing information.
+I am interested in learning data science, big-data, mechine learning, user experience and digital marketing. 
+My life goal is to contribute to education and financial innovation in Africa. I currently code using {html} {css} {python} {java}""")
+    pdf.heading('Education')
+    
 
-    # pdf.cell(40, 10, f"First Name: {name}", ln=True)
-    # pdf.cell(40, 10, f" Hi {name}", ln=True)
-    # pdf.cell(40, 10, f"You are {age} years old", ln=True)
-    # pdf.cell(40, 10, "Im here to do your home work", ln=True)
+
+
+      
+   
+
+
     ap = pdf.output("tmp/my_cv.pdf")
 
 
